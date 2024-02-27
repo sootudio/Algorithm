f8e366f17c9939d08ddf042bc854b47945afb3eb
Kim, Sooyoung <83462874+sootudio@users.noreply.github.com>
2024-02-27 17:28:33 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15504 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -20,40 +20,22 @@ public class Main {
 	}
 
 	private static int dp() {
-		
 		if(N == 1) return 0;
-		else if(N == 2 || N == 3) return 1;
-		
+		else if(N == 2 || N ==3) return 1;
 		makeOne[1] = 0;
 		makeOne[2] = 1;
 		makeOne[3] = 1;
 
 		for(int i = 4 ; i <= N ; i++) {
-			if(i % 6 == 0) {
-				makeOne[i] = Math.min(Math.min(makeOne[i/3] + 1, makeOne[i/2] + 1), makeOne[i-1]+1) ;
+			makeOne[i] = makeOne[i-1] + 1; // 현재값에서 1을 뺀 경우를 최소값으로 설정해 놓음
+			// 2로 나눠 떨어질때, 2로 나눈 결과와 1을 뺀 결과를 비교하여 작은 것으로 설정한다.
+			// 위에서 -1 을 뺀 경우를 이미 최솟값으로 해 놓았으므로, makeOne[i]로 비교해도 무방함.
+			if(i%2 == 0) {
+				makeOne[i] = Math.min(makeOne[i/2]+1, makeOne[i]);
 			}
-			else {
-				switch(i % 3) {
-				case 0:
-					makeOne[i] = Math.min(makeOne[i/3] + 1,  makeOne[i-1]+1); 
-					break;
-				case 1:
-					if(i % 2 == 0) {
-						makeOne[i] = Math.min(makeOne[i/2] + 1,  makeOne[i-1]+1); 
-					}
-					else if(i % 2 == 1) {
-						makeOne[i] = makeOne[i-1] + 1;
-					}
-					break;
-				case 2: 
-					if(i % 2 == 0) {
-						makeOne[i] = Math.min(makeOne[i/2] + 1,  makeOne[i-1]+1); 
-					}
-					else if(i % 2 == 1) {
-						makeOne[i] = makeOne[i-1] + 1;
-					}
-					break;
-				}
+			// 3으로 나눠 떨어질 때, 3으로 나눈 결과와 1을 뺀 결과를 비교하여 작은 것으로 설정한다.
+			if(i%3 == 0) {
+				makeOne[i] = Math.min(makeOne[i/3]+1, makeOne[i]);
 			}
 		}
 		
