963f6a03129252956e9a0be9a50e406d2e79a768
Kim, Sooyoung <83462874+sootudio@users.noreply.github.com>
2024-03-30 23:50:39 +0900

    [Silver IV] Title: 스택, Time: 144 ms, Memory: 16840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cmd = Integer.parseInt(br.readLine());
+		
+		Deque<Integer> stack = new ArrayDeque<>();
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0 ; i<cmd ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String cmd1 = st.nextToken();
+			
+			switch (cmd1) {
+			case "push": {
+				int value = Integer.parseInt(st.nextToken());
+				stack.push(value);
+				break;
+			}
+			case "pop": {
+				if(stack.isEmpty())
+					sb.append(-1).append("\n");
+				else
+					sb.append(stack.pop()).append("\n");
+				break;
+			}
+			case "size": {
+				sb.append(stack.size()).append("\n");
+				break;
+			}
+			case "empty": {
+				if(stack.isEmpty())
+					sb.append(1).append("\n");
+				else
+					sb.append(0).append("\n");
+				break;
+			}
+			case "top": {
+				if(stack.isEmpty())
+					sb.append(-1).append("\n");
+				else
+					sb.append(stack.peek()).append("\n");
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + cmd1);
+			}
+		}
+		if(sb.length() > 0) sb.setLength(sb.length()-1);
+		System.out.println(sb);
+	}
+
+}
