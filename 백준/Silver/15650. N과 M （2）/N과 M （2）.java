730059d240f7bdc99437d7fcc10ef069dabe841a
Kim, Sooyoung <83462874+sootudio@users.noreply.github.com>
2024-04-11 13:26:23 +0900

    [Silver III] Title: N과 M (2), Time: 84 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
-	static int[] sequence;
+	static int[] picked;
 	static boolean[] isVisited;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,25 +16,25 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		sequence = new int[M];
+		picked = new int[M];
 		isVisited = new boolean[N+1];
 		
-		combi(1, 0);
+		permu(1, 0);
 	}
 
-	private static void combi(int idx, int cnt) {
+	private static void permu(int idx, int cnt) {
 		if(cnt == M) {
-			for(int num : sequence) {
-				System.out.print(num + " ");
+			for(int i = 0 ; i < M ; i++) {
+				System.out.print(picked[i]+" ");
 			}
 			System.out.println();
 			return;
 		}
-		for(int i = idx ; i<= N ; i++) {
-			
-			sequence[cnt] = i;
-			combi(i+1 , cnt + 1);
+		for(int i = idx ; i <= N ; i++) {
+			picked[cnt] = i; 
+			permu(i+1, cnt+1);
 		}
+		
 	}
 
 }
