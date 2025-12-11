a6b827fa5b9e42236c4c73eb851cbd191e5337a3
Kim, Sooyoung <83462874+sootudio@users.noreply.github.com>
2025-12-11 22:11:11 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+        
+        while(N >= 5){
+            result += N/5;
+            N /= 5;
+        }
+
+        System.out.println(result);
+    }
+}
